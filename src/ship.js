5daa0ec9a8476adb594ed8b1a781fed7b0df42d6
Lydos <lydossky@gmail.com>
2024-11-15 18:18:57 +0300

    refactor: ship object fields are now private

--- src/ship.js
+++ src/ship.js
@@ -1,22 +1,24 @@
 export default class Ship {
+  #hits;
+  #length;
   constructor(length) {
-    this.hits = 0;
-    this.length = length;
+    this.#hits = 0;
+    this.#length = length;
   }
 
   getLength() {
-    return this.length;
+    return this.#length;
   }
 
   hit() {
-    this.hits += 1;
+    this.#hits += 1;
   }
 
   getHits() {
-    return this.hits;
+    return this.#hits;
   }
 
   isSunk() {
-    return this.hits === this.length;
+    return this.#hits === this.#length;
   }
 }
