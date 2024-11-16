7996065369fad920786013fe7f8171490747252f
Lydos <lydossky@gmail.com>
2024-11-16 16:53:43 +0300

    feat: ships have types field

--- src/ship.js
+++ src/ship.js
@@ -1,21 +1,27 @@
 export default class Ship {
   #hits;
   #length;
-  constructor(length) {
+  #shipType;
+  constructor(shipType, length) {
     this.#hits = 0;
     this.#length = length;
+    this.#shipType = shipType;
   }
 
   getLength() {
     return this.#length;
   }
 
-  hit() {
-    this.#hits += 1;
-  }
-
   getHits() {
     return this.#hits;
+  }
+
+  getShipType() {
+    return this.#shipType;
+  }
+
+  hit() {
+    this.#hits += 1;
   }
 
   isSunk() {
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,11 +4,11 @@ describe("Testing ship functionality", function () {
   let carrier, battleship, cruiser, submarine, destroyer;
 
   beforeEach(function () {
-    carrier = new Ship(5);
-    battleship = new Ship(4);
-    cruiser = new Ship(3);
-    submarine = new Ship(3);
-    destroyer = new Ship(2);
+    carrier = new Ship("carrier", 5);
+    battleship = new Ship("battleship", 4);
+    cruiser = new Ship("cruiser", 3);
+    submarine = new Ship("submarine", 3);
+    destroyer = new Ship("destroyer", 2);
   });
 
   test("You can create a ship with given length", function () {
@@ -17,6 +17,14 @@ describe("Testing ship functionality", function () {
     expect(cruiser.getLength()).toBe(3);
     expect(submarine.getLength()).toBe(3);
     expect(destroyer.getLength()).toBe(2);
+  });
+
+  test("You can get ships type", function () {
+    expect(carrier.getShipType()).toEqual("carrier");
+    expect(battleship.getShipType()).toEqual("battleship");
+    expect(cruiser.getShipType()).toEqual("cruiser");
+    expect(submarine.getShipType()).toEqual("submarine");
+    expect(destroyer.getShipType()).toEqual("destroyer");
   });
 
   test("You can hit a ship and learn the number of hits on it.", function () {
