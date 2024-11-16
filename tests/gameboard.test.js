6f9dc8d0124b7bb23f71a2f04b0f66a5153cb569
Lydos <lydossky@gmail.com>
2024-11-16 19:51:21 +0300

    test: gameboard new way of testing with mock

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,13 +1,44 @@
 import GameBoard from "../src/gameboard";
 
-describe("Testing Gameboard functionality", function () {
-  let gameBoard;
+const BOARD = [
+  [0, 0, 0, 0, 0, "carrier", 0, 0, 0, 0],
+  [
+    "battleship",
+    "battleship",
+    "battleship",
+    "battleship",
+    0,
+    "carrier",
+    0,
+    0,
+    0,
+    0,
+  ],
+  [0, 0, 0, 0, 0, "carrier", 0, 0, 0, 0],
+  ["destroyer", 0, 0, 0, 0, "carrier", 0, 0, 0, 0],
+  ["destroyer", 0, 0, 0, 0, "carrier", "cruiser", 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, "cruiser", 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, "cruiser", 0, "submarine", 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, "submarine", 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, "submarine", 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-  beforeEach(function () {
-    gameBoard = new GameBoard();
-  });
+jest.mock("../src/gameboard");
+
+GameBoard.mockImplementation(() => ({
+  board: BOARD,
+  getBoard: jest.fn(() => BOARD),
+}));
+
+describe("Testing Gameboard functionality", function () {
+  const gameBoard = new GameBoard();
 
   test("Gameboard object can be created", function () {
-    expect(gameBoard.getBoard().length).toEqual(10);
+    expect(gameBoard.getBoard()[1]).toContain("battleship");
+    expect(gameBoard.getBoard()[0]).toContain("carrier");
+    expect(gameBoard.getBoard()[3]).toContain("destroyer");
+    expect(gameBoard.getBoard()[7]).toContain("submarine");
+    expect(gameBoard.getBoard()[6]).toContain("cruiser");
   });
 });
