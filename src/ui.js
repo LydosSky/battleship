3723838a9fca9c1c75cf945a9e2ca44f1a2e0cfa
Lydos <lydossky@gmail.com>
2024-11-17 16:57:02 +0300

    feat: ui drawer implementation

---
+++ src/ui.js
@@ -0,0 +1,32 @@
+const UI = (function () {
+  const BOARDSIZE = 10;
+
+  function drawBoard(board, player) {
+    const playerBoard = player.gameBoard.getBoard();
+
+    for (let row = 0; row < BOARDSIZE; row++) {
+      for (let col = 0; col < BOARDSIZE; col++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.setAttribute("row", row);
+        square.setAttribute("col", col);
+        if (playerBoard[row][col] !== 0) {
+          square.classList.add(playerBoard[row][col]);
+        }
+        if (playerBoard[row][col] === -1) {
+          square.classList.add("miss");
+        }
+
+        if (playerBoard[row][col] === 1) {
+          square.classList.add("hit");
+        }
+
+        board.appendChild(square);
+      }
+    }
+  }
+
+  return { drawBoard };
+})();
+
+export default UI;
